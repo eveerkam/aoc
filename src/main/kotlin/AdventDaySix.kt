af0144e22f35a754163c2b191e2bc02b5e48938f
Erik Veerkamp <eveerkamp@vxcompany.com>
2019-12-06 17:55:44 +0100

    day 6 - impl part two - to be cleaned

--- src/main/kotlin/AdventDaySix.kt
+++ src/main/kotlin/AdventDaySix.kt
@@ -15,37 +15,103 @@ class AdventDaySix(private var path: String) {
     }
 
     fun run() {
-        println("Day 6 - Part one: " + partOne())
+        // println("Day 6 - Part one: " + partOne())
+        println("Day 6 - Part two: " + partTwo())
     }
 
-    private fun partOne(): Int {
+    private fun partTwo(): Any? {
         val orbits = input.map { Pair(it.split(")")[0], it.split(")")[1]) }
-        val allOrbits = mutableMapOf<String, MutableList<String>>()
+        val orbitsMap = mutableMapOf<String, MutableList<String>>()
+
         orbits.forEach {
-            if (allOrbits.containsKey(it.second)) {
-                allOrbits[it.second]!!.add(it.first)
-                allOrbits[it.second] = allOrbits[it.second]!!
+            if (orbitsMap.containsKey(it.second)) {
+                orbitsMap[it.second]!!.add(it.first)
+                orbitsMap[it.second] = orbitsMap[it.second]!!
             } else {
-                allOrbits[it.second] = mutableListOf(it.first)
+                orbitsMap[it.second] = mutableListOf(it.first)
             }
         }
 
-        allOrbits.forEach {
+        var pathToMe =mutableListOf<String>()
+        orbitsMap.filter { it.key == "YOU" }.forEach {
             it.value.forEach { child ->
-                getOrbits(allOrbits, child)
+                pathToMe = getOrbitsTwo(orbitsMap, child, pathToMe)
+            }
+        }
+        pathToMe.forEachIndexed {idx, it -> println("$idx $it") }
+        println("---")
+       counter = 0
+
+        var pathToSanta = mutableListOf<String>()
+        orbitsMap.filter { it.key == "SAN" }.forEach {
+            it.value.forEach { child ->
+                pathToSanta = getOrbitsTwo(orbitsMap, child,pathToSanta)
+            }
+        }
+
+        pathToSanta.forEachIndexed {idx, it -> println("$idx $it") }
+
+        var lowestMeIndex = pathToMe.size
+        pathToMe.forEachIndexed {meI, me ->
+            pathToSanta.forEachIndexed{santaI, santa ->
+                    if(me == santa && meI < lowestMeIndex){
+                        println(santa)
+                        println(me)
+                        println(meI + santaI)
+                        lowestMeIndex = meI
+                    }
+            }
+         }
+        return counter
+    }
+
+    private fun getOrbitsTwo(
+        orbitsMap: MutableMap<String, MutableList<String>>,
+        subOrbit: String,
+        pathToMe: MutableList<String>
+    ):MutableList<String>{
+        if (orbitsMap.containsKey(subOrbit)) {
+            orbitsMap[subOrbit]!!.forEach {
+                counter++
+                pathToMe.add(subOrbit)
+                getOrbitsTwo(orbitsMap, it, pathToMe)
+            }
+        } else {
+            pathToMe.add(subOrbit)
+            counter++
+        }
+        return pathToMe
+    }
+
+
+    private fun partOne(): Int {
+        val orbits = input.map { Pair(it.split(")")[0], it.split(")")[1]) }
+        val orbitsMap = mutableMapOf<String, MutableList<String>>()
+        orbits.forEach {
+            if (orbitsMap.containsKey(it.second)) {
+                orbitsMap[it.second]!!.add(it.first)
+                orbitsMap[it.second] = orbitsMap[it.second]!!
+            } else {
+                orbitsMap[it.second] = mutableListOf(it.first)
+            }
+        }
+
+        orbitsMap.forEach {
+            it.value.forEach { child ->
+                getOrbits(orbitsMap, child)
             }
         }
         return counter
     }
 
     private fun getOrbits(
-        allOrbits: MutableMap<String, MutableList<String>>,
+        orbitsMap: MutableMap<String, MutableList<String>>,
         subOrbit: String
     ) {
-        if (allOrbits.containsKey(subOrbit)) {
-            allOrbits[subOrbit]!!.forEach {
+        if (orbitsMap.containsKey(subOrbit)) {
+            orbitsMap[subOrbit]!!.forEach {
                 counter++
-                getOrbits(allOrbits, it)
+                getOrbits(orbitsMap, it)
             }
         } else {
             counter++
