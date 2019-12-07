cfe3b3fb3e64e00c11fe40e3c62268329686a0a8
Erik Veerkamp <eveerkamp@vxcompany.com>
2019-12-07 12:44:18 +0100

    day 6 - impl part two - clean

--- src/main/kotlin/AdventDaySix.kt
+++ src/main/kotlin/AdventDaySix.kt
@@ -15,106 +15,70 @@ class AdventDaySix(private var path: String) {
     }
 
     fun run() {
-        // println("Day 6 - Part one: " + partOne())
+        println("Day 6 - Part one: " + partOne())
         println("Day 6 - Part two: " + partTwo())
     }
 
-    private fun partTwo(): Any? {
-        val orbits = input.map { Pair(it.split(")")[0], it.split(")")[1]) }
-        val orbitsMap = mutableMapOf<String, MutableList<String>>()
-
-        orbits.forEach {
-            if (orbitsMap.containsKey(it.second)) {
-                orbitsMap[it.second]!!.add(it.first)
-                orbitsMap[it.second] = orbitsMap[it.second]!!
-            } else {
-                orbitsMap[it.second] = mutableListOf(it.first)
-            }
-        }
-
-        var pathToMe =mutableListOf<String>()
-        orbitsMap.filter { it.key == "YOU" }.forEach {
-            it.value.forEach { child ->
-                pathToMe = getOrbitsTwo(orbitsMap, child, pathToMe)
-            }
-        }
-        pathToMe.forEachIndexed {idx, it -> println("$idx $it") }
-        println("---")
-       counter = 0
-
-        var pathToSanta = mutableListOf<String>()
-        orbitsMap.filter { it.key == "SAN" }.forEach {
-            it.value.forEach { child ->
-                pathToSanta = getOrbitsTwo(orbitsMap, child,pathToSanta)
-            }
-        }
-
-        pathToSanta.forEachIndexed {idx, it -> println("$idx $it") }
-
-        var lowestMeIndex = pathToMe.size
-        pathToMe.forEachIndexed {meI, me ->
-            pathToSanta.forEachIndexed{santaI, santa ->
-                    if(me == santa && meI < lowestMeIndex){
-                        println(santa)
-                        println(me)
-                        println(meI + santaI)
-                        lowestMeIndex = meI
-                    }
-            }
-         }
-        return counter
-    }
-
-    private fun getOrbitsTwo(
-        orbitsMap: MutableMap<String, MutableList<String>>,
-        subOrbit: String,
-        pathToMe: MutableList<String>
-    ):MutableList<String>{
-        if (orbitsMap.containsKey(subOrbit)) {
-            orbitsMap[subOrbit]!!.forEach {
-                counter++
-                pathToMe.add(subOrbit)
-                getOrbitsTwo(orbitsMap, it, pathToMe)
-            }
-        } else {
-            pathToMe.add(subOrbit)
-            counter++
-        }
-        return pathToMe
-    }
-
-
     private fun partOne(): Int {
-        val orbits = input.map { Pair(it.split(")")[0], it.split(")")[1]) }
-        val orbitsMap = mutableMapOf<String, MutableList<String>>()
-        orbits.forEach {
-            if (orbitsMap.containsKey(it.second)) {
-                orbitsMap[it.second]!!.add(it.first)
-                orbitsMap[it.second] = orbitsMap[it.second]!!
-            } else {
-                orbitsMap[it.second] = mutableListOf(it.first)
-            }
-        }
-
+        val orbitsMap = getOrbitsMap()
         orbitsMap.forEach {
             it.value.forEach { child ->
-                getOrbits(orbitsMap, child)
+                getOrbits(orbitsMap, child, mutableListOf())
             }
         }
         return counter
+    }
+
+    private fun partTwo(): Any? {
+        val pathToMe = getMapToOrbit("YOU")
+        val pathToSanta = getMapToOrbit( "SAN")
+
+        val firstMatchingOrbit = pathToMe.filter { me -> pathToSanta.any { it == me } }.first()
+        val indexMeFirstMatch = pathToMe.indexOf(firstMatchingOrbit)
+        val indexSantaFirstMatch = pathToSanta.indexOf(firstMatchingOrbit)
+
+        return indexMeFirstMatch + indexSantaFirstMatch
     }
 
     private fun getOrbits(
         orbitsMap: MutableMap<String, MutableList<String>>,
-        subOrbit: String
-    ) {
+        subOrbit: String,
+        pathToMe: MutableList<String> // partTwo
+    ): MutableList<String> {
         if (orbitsMap.containsKey(subOrbit)) {
             orbitsMap[subOrbit]!!.forEach {
                 counter++
-                getOrbits(orbitsMap, it)
+                pathToMe.add(subOrbit) // partTwo
+                getOrbits(orbitsMap, it, pathToMe)
             }
         } else {
+            pathToMe.add(subOrbit) // partTwo
             counter++
         }
+        return pathToMe // partTwo
     }
+
+    private fun getOrbitsMap(): MutableMap<String, MutableList<String>> {
+        val orbits = input.map { Pair(it.split(")")[0], it.split(")")[1]) }
+        val orbitsMap = mutableMapOf<String, MutableList<String>>()
+
+        orbits.forEach {
+            if (orbitsMap.containsKey(it.second)) {
+                orbitsMap[it.second]!!.add(it.first)
+                orbitsMap[it.second] = orbitsMap[it.second]!!
+            } else {
+                orbitsMap[it.second] = mutableListOf(it.first)
+            }
+        }
+        return orbitsMap
+    }
+
+    fun getMapToOrbit(key: String): MutableList<String> {
+        var pathToOrbit = mutableListOf<String>()
+        getOrbitsMap()[key]!!.forEach { orbit ->
+            pathToOrbit = getOrbits( getOrbitsMap(),orbit, pathToOrbit)
+        }
+        return pathToOrbit
+    }
+
 }
